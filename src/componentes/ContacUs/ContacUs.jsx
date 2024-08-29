88f78d6368ddc789ff942dc1787f3f9d35403313
Jesus <6.jesus.a@gmail.com>
2024-08-28 23:08:19 -0400

    comenzarndo con contact us

---
+++ src/componentes/ContacUs/ContacUs.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Container, Form, Map } from "./styled";
+
+const ContacUs = () => {
+  return (
+    <Container>
+      <Map>
+      </Map>
+      <Form />
+    </Container>
+  );
+};
+
+export default ContacUs;
